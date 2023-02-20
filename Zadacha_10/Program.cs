39a030eec54af8b09fcb210bbd6f876ef91f2033
ilnur-Git <mazel08@yandex.ru>
2023-02-21 00:24:41 +0300

    домашка второй семинар

---
+++ Zadacha_10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number <99 || number > 999)
+{
+    Console.WriteLine("Ошибка! ВВедите трехзначное число");
+}
+else
+{
+    int digit2 = (number / 10) % 10;
+    Console.WriteLine($"{number} вторая цифра: {digit2}");
+}
+
+
---
+++ Zadacha_13/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+
+/////
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100 )
+{
+    Console.WriteLine("Число не трехзначное");
+}
+
+else 
+{
+    while (number >999)
+    {
+    number/=10;
+    }
+    System.Console.WriteLine($"{(number % 10)}");
+}
+
